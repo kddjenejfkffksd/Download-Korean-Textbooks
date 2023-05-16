a1d60cef28b9f4c517e6f0a67eddd5ac6344c6c6
Iso Lee <coughingmouse@users.noreply.github.com>
2023-05-16 16:36:22 +0900

    Update download_korean_textbooks.py

--- download_korean_textbooks.py
+++ download_korean_textbooks.py
@@ -34,8 +34,8 @@ def is_notebook() -> bool:
 if not is_notebook():
     os.system('mkdir "Korean Textbooks"')
     os.chdir("Korean Textbooks")
-    os.system("zsh -c python3 -m venv .")
-    os.system("zsh -c source ./bin/activate && pip install requests fpdf pick")
+    os.system("/usr/bin/python3 -m venv .")
+    os.system("source ./bin/activate && pip install requests fpdf pick")
 
 
 ## Importing Necessary Modules
